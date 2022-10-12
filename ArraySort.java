bc6707811bbc89661c4523d7de400e5c67827d96
chitraaruna09@gmail.com <chitraaruna09@gmail.com>
2022-10-12 11:57:03 +0530

    New Programs added

---
+++ ArraySort.java
@@ -0,0 +1,18 @@
+//Bubble Sort Program
+class ArraySort{
+	public static void main(String args[]){
+		int a[] = {9,90,23,4,5};
+		int temp;
+		for(int i=0;i<a.length;i++){
+			for(int j=i+1;j<a.length;j++){
+				if(a[i]>a[j]){
+					temp = a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+			System.out.println(a[i]);
+		}
+		
+	}
+}
